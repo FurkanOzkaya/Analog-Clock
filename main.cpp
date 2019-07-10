c04db740b6363101a4c56edb82e8d5e66309f324
Furkan Ozkaya <arif.ozkaya@ogrenci.selcuk.edu.tr>
2019-07-10 22:53:09 +0300

    10.07.2019

--- main.cpp
+++ main.cpp
@@ -9,12 +9,11 @@
  	initwindow(600,600,"Anolog_saat");
  	
  
-     // Saat Koordinatları
     
     int koordinatX[12] = {50, 87, 100, 87, 50, 0, -50, -87, -100, -87, -50, 0};
     int koordinatY[12] = {-87, -50, 0, 50, 87, 100, 87, 50, 0, -50, -87, -100};
     
-    // Dakika ve Saniye Koordinatları
+  
     
     int SaniyeveDakikaX[60] = {0, 18, 35, 53, 69, 85, 100, 114, 126, 138, 147, 155, 162, 166, 169, 170,
                                   169, 166, 162, 155, 147, 138, 126, 114, 100, 85, 69, 53, 35, 18, 0,
@@ -30,13 +29,12 @@
  	
  	
  	
- 	 //süre ile ilgili değişkenlerde
  	 
  	 time_t Szaman;	
  	 struct tm * simdikizaman;	
  	 char a[100];
  	
-	 //tarih
+
 	 time_t t;
  	struct tm *zaman;
  	time(&t);
@@ -45,7 +43,6 @@
 	  
 	  
 	  
-	  //grafiğin ortasına almak için
  	
 	  int MerkezX =getmaxx()/2;
  	 int MerkezY=getmaxy()/2;
@@ -54,14 +51,12 @@
  	 
  	 for(int i=1;  ;i++)
  	 {
-	  
- 	 //s�re ile ilgili fonksiyon
+
  	 
  	 Szaman = time(NULL);
 	 simdikizaman = localtime(&Szaman);
 	 
 	 
-	 //cenber çizelim 
 	 if(i%15==0)
 	 {
 	     i++;
@@ -95,7 +90,7 @@
 	  outtextxy(MerkezX-100,MerkezY+50,asctime(zaman));
  	 
  	
-	 strftime(a, 100, "%I", simdikizaman);
+	strftime(a, 100, "%I", simdikizaman);
     sscanf(a, "%d", &Sds);
     setcolor(RED);
     line( MerkezX, MerkezY, MerkezX+koordinatX[Sds-1], MerkezY+koordinatY[Sds-1]);
